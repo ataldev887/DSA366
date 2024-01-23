e27f885a7aa72812dbffa72a50ca071f67219613
dev atal <134199050+ataldev887@users.noreply.github.com>
2024-01-23 22:02:29 +0530

    OOPS-1

---
+++ src/lecture35/Student.java
@@ -0,0 +1,14 @@
+package lecture35;
+
+public class Student {
+	
+	String name;
+	int age;
+	
+	public void Intro_yourSelf() {
+		String name="Kaju";
+		System.out.println("My name is "+this.name+" and age is "+this.age);
+		//if we use this class will be preferred otherwise local variable
+	}
+
+}
