f88adb472e0df4acc7ad00585338333e12bc691b
dev atal <134199050+ataldev887@users.noreply.github.com>
2024-01-26 00:46:11 +0530

    Stack

---
+++ src/lecture39/Stack_Reverse.java
@@ -0,0 +1,29 @@
+package lecture39;
+
+import java.util.Stack;
+
+public class Stack_Reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<Integer> st = new Stack<>();
+		st.push(10);
+		st.push(20);
+		st.push(30);
+		st.push(40);
+		st.push(50);
+		System.out.println(st);
+		System.out.println("*********");
+		Reverse(st);
+		System.out.println(st);
+	}
+	public static void Reverse(Stack<Integer> st) {
+		if(st.isEmpty()) {
+			return;
+		}
+		int x = st.pop();
+		Reverse(st);
+		st.push(x);
+	}
+
+}
